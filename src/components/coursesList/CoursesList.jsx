97120317b6f51685c64e56d74538c23216a39155
aditiarya37 <aditiarya1511@gmail.com>
2025-02-01 19:04:15 +0530

    modified the play icon on courses list page

--- src/components/coursesList/CoursesList.jsx
+++ src/components/coursesList/CoursesList.jsx
@@ -1,56 +1,56 @@
 import React from "react";
 import "./CoursesList.css";
-import { FaPlay } from "react-icons/fa";
-import { FaLock } from "react-icons/fa6";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlay, faLock } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom"; // For navigation
 
 const courses = [
   {
     title: "Introduction of Debit and Credit Cards",
     unlocked: true,
-    icon: <FaPlay className="play-icon" />,
+    icon: <FontAwesomeIcon icon={faPlay} className="play-icon" />,
     link: "/roadmap/Introduction of Debit and Credit Cards",
   },
   {
     title: "What is Personal Finance?",
     unlocked: false,
-    icon: <FaLock className="lock-icon" />,
+    icon: <FontAwesomeIcon icon={faLock} className="lock-icon" />,
     link: "/roadmap/What is Personal Finance?",
   },
   {
     title: "Definition and Cryptocurrency as a Digital Asset",
     unlocked: false,
-    icon: <FaLock className="lock-icon" />,
+    icon: <FontAwesomeIcon icon={faLock} className="lock-icon" />,
     link: "/roadmap/Definition and Cryptocurrency as a Digital Asset",
   },
   {
     title: "Understanding Interest Rates",
     unlocked: false,
-    icon: <FaLock className="lock-icon" />,
+    icon: <FontAwesomeIcon icon={faLock} className="lock-icon" />,
     link: "/roadmap/Understanding Interest Rates",
   },
   {
     title: "Investing for Beginners",
     unlocked: false,
-    icon: <FaLock className="lock-icon" />,
+    icon: <FontAwesomeIcon icon={faLock} className="lock-icon" />,
     link: "/roadmap/Investing for Beginners",
   },
   {
     title: "Identity Theft Protection",
     unlocked: false,
-    icon: <FaLock className="lock-icon" />,
+    icon: <FontAwesomeIcon icon={faLock} className="lock-icon" />,
     link: "/roadmap/Identity Theft Protection",
   },
   {
     title: "How to Save Money",
     unlocked: false,
-    icon: <FaLock className="lock-icon" />,
+    icon: <FontAwesomeIcon icon={faLock} className="lock-icon" />,
     link: "/roadmap/How to Save Money",
   },
   {
     title: "Tax Basics",
     unlocked: false,
-    icon: <FaLock className="lock-icon" />,
+    icon: <FontAwesomeIcon icon={faLock} className="lock-icon" />,
     link: "/roadmap/Tax Basics",
   },
 ];
